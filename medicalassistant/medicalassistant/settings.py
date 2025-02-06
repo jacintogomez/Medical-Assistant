ce81b4ac7fc406a6a864da36fede3ccbc5eb8aea
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2025-02-05 21:01:25 -0500

    added patientdata to installed apps

--- medicalassistant/medicalassistant/settings.py
+++ medicalassistant/medicalassistant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tnqw2h6!)53*9c$t4q&q4x$sr_2&uuy0)rx7tqouj8@%6mk(&r'
+#TODO REPLACE WITH THIS: SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY='django-insecure-tnqw2h6!)53*9c$t4q&q4x$sr_2&uuy0)rx7tqouj8@%6mk(&r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'patientdata',
 ]
 
 MIDDLEWARE = [
